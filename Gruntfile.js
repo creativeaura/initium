be4c51c70d3884445db933cd2788a585dcfc4b41
Gaurav Jassal <gauravjassal@gmail.com>
2013-10-04 10:55:18 +0100

    more modules added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,22 @@
+/*global module*/
+/*jshint camelcase: true, indent: 2  */
+
 /*!
  * Initium
  * @author Gaurav Jassal
  */
 
-'use strict';
 
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
+  'use strict';
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     /**
-     * Project banner
-     * Dynamically appended to CSS/JS files
-     * Inherits text from package.json
+     * Create dynamic banner
      */
-    tag: {
+    info: {
       banner: '/*!\n' +
               ' * <%= pkg.name %>\n' +
               ' * <%= pkg.title %>\n' +
@@ -26,23 +27,40 @@ module.exports = function(grunt) {
               ' */\n'
     },
 
+    /**
+     * Watch various task on file modification
+     */
     watch: {
       less: {
-        files: ['app/css/less/**/*.less'],
-        tasks: ['less'],
+        files:   ['app/css/less/**/*.less'],
+        tasks:   ['less'],
         options: {
-          spawn: false,
+          spawn:      false,
           livereload: true
         }
       },
       html: {
-        files: ['app/*.html'],
+        files:   ['app/*.html'],
         options: {
           livereload: true
         }
+      },
+      coffee: {
+        files:   ['app/js/modules/**/*.coffee'],
+        tasks:   ['coffee'],
+        options: {
+          livereload: true
+        }
+      },
+      scripts: {
+        files:   ['app/js/**/*.js'],
+        tasks:   ['jshit']
       }
     },
 
+    /**
+     * Compile all less files to single css file
+     */
     less: {
       development: {
         options: {
@@ -54,7 +72,7 @@ module.exports = function(grunt) {
       },
       production: {
         options: {
-          paths: ['app/css/less'],
+          paths:       ['app/css/less'],
           yuicompress: true
         },
         files: {
@@ -63,31 +81,97 @@ module.exports = function(grunt) {
       }
     },
 
+    /**
+     * Run local server for development with livereload
+     */
     connect: {
       development: {
         options: {
-          port: 9001,
-          base: 'app',
+          port:      9001,
+          base:      'app',
           keepalive: true
         }
       },
       production: {
         options: {
-          port: 9002,
-          base: 'production',
+          port:      9002,
+          base:      'production',
           keepalive: true
         }
       }
     },
 
+    /**
+     * Compile all commonjs dependencies to single bundle file.
+     */
     requirejs: {
       compile: {
         options: {
-          name: 'config',
-          baseUrl: 'app/js',
+          name:           'config',
+          baseUrl:        'app/js',
           mainConfigFile: 'app/js/config.js',
-          out: 'app/js/bundle.js'
+          out:            'app/js/bundle.js'
         }
+      }
+    },
+
+    /**
+     * Comple all of your coffeescript file into javascript
+     */
+    coffee: {
+      files:            ['app/js/modules/**/*.coffee'],
+      glob_to_multiple: {
+        expand:  true,
+        flatten: true,
+        cwd:     'app/js/modules/',
+        src:     ['*.coffee'],
+        dest:    'app/js/modules/',
+        ext:     '.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true,
+          module: true,
+          require: true
+        },
+        ignores: ['app/resources/**/*.js', 'app/js/plugins/**/*.js']
+      },
+      files: {
+        src: ['Gruntfile.js', 'app/**/*.js']
+      }
+    },
+    copy: {
+      production: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**'], dest: 'production/'}
+        ]
+      }
+    },
+    // clean: [
+    //   'production/less',
+    //   'production/scripts/plugins/**',
+    //   'production/scripts/app.js',
+    //   'production/scripts/config.js'
+    //   ],
+    usemin: {
+      html: ['production/*.html']
+    },
+    ftp: {
+      build: {
+        auth: {
+          host: 'server.com',
+          port: 21,
+          authKey: 'key1'
+        },
+        src: 'production',
+        dest: '/path/to/destination/folder',
+        exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db', 'dist/tmp']
       }
     }
   });
@@ -96,8 +180,10 @@ module.exports = function(grunt) {
   // Dynamically load npm tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('server', ['connect:development']);
+  grunt.registerTask('default',    ['jshint', 'watch']);
+  grunt.registerTask('build',      ['less', 'coffee', 'copy:production', 'clean', 'usemin']);
+  grunt.registerTask('deploy',     ['less', 'coffee', 'copy:production', 'clean', 'usemin', 'ftp']);
+  grunt.registerTask('server',     ['connect:development']);
   grunt.registerTask('production', ['connect:production']);
 
 };
---
+++ files
@@ -0,0 +1,6 @@
+(function() {
+  var a;
+
+  a = 66;
+
+}).call(this);
